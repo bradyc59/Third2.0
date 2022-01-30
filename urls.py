4071d4808421fe90771d3ef87bb7b1ec1e3d99fd
Conor Brady <81228574+bradyc59@users.noreply.github.com>
2022-01-30 22:57:54 +0000

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"), # /app
-    path('registration/', views.register, name="register"),
-    path('signup/', views.CaUserSignupView.as_view(), name="signup"),
-    path('game', views.GameView.as_view(), name="game")
-]
